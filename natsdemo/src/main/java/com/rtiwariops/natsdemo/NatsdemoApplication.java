bb52a194a2a3b11ddd6330e4a601c84a9e41bccd
Ravi <rtiwari@paylynxs.com>
2020-09-27 22:20:28 -0600

    created spring boot skeleton project with jnats dependencies

---
+++ natsdemo/src/main/java/com/rtiwariops/natsdemo/NatsdemoApplication.java
@@ -0,0 +1,34 @@
+package com.rtiwariops.natsdemo;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.nats.client.Connection;
+import io.nats.client.Dispatcher;
+import io.nats.client.Nats;
+
+@SpringBootApplication
+public class NatsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsdemoApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			Connection conn = Nats.connect();
+			Dispatcher dispather = conn.createDispatcher(message -> {
+				System.out.printf(String.format("Received message: [%s] from [%s] : successfully",
+						new String(message.getData(), StandardCharsets.UTF_8), message.getSubject()));
+			});
+			dispather.subscribe("rtiwariops");
+		};
+
+	}
+
+}
